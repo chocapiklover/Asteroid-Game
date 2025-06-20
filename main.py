234d72ac2b51ea26dc93cb1cd654c25707f5f55a
chocapiklover <a.harmuth33@gmail.com>
2025-06-20 19:39:12 +0200

    added rotation and movement for triangle

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        
+        dt = clock.tick(60)/1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("black")
-            pygame.display.flip()
+        
+        screen.fill("black")
+
+
+        for sprite in updatable:
+            sprite.update(dt)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
